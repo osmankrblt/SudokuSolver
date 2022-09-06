71c8592af63181d2c60f9fb970bf05321063ba26
Osman Karabulut <h.osmankarabulut.fb67@hotmail.com>
2022-09-06 19:32:53 +0300

    End of the Project, for now

--- sudokuSensor.py
+++ sudokuSensor.py
@@ -1,29 +1,27 @@
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
-from matplotlib import pyplot as plt
 import tensorflow as tf
-
+import sudokuSolver as ss
+from matplotlib import pyplot as plt
 
 sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
 
-
 model = load_model('model.h5') 
+
 class SudokuClassifier:
     def __init__(self):
        
-        
         self.numberList=[]
+        self.posArray=[]
    
     def preprocessSudoku(self,sudoku):
-        gray = cv2.cvtColor(sudoku,cv2.COLOR_BGR2GRAY)
-        blur =  cv2.medianBlur(gray,3)
+        sudoku = cv2.cvtColor(sudoku,cv2.COLOR_BGR2GRAY)
+        sudoku =  cv2.medianBlur(sudoku,3)
         
-        thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,8)
-        opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, (5,5))
-        closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, (5,5))
-
-        return closing
+        sudoku = cv2.adaptiveThreshold(sudoku,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,8)
+        
+        return sudoku
 
     def findBox(self,problem,procesedImage):
         edge = cv2.Canny(procesedImage, 175, 175)
@@ -35,65 +33,62 @@ class SudokuClassifier:
 
         x,y,w,h = cv2.boundingRect(cnt)
 
-        cv2.rectangle(problem,(x,y),(x+w,y+h),(0,255,0),1)
-        
+        cv2.rectangle(problem,(x,y),(x+w,y+h),(0,255,0),5)
+        cv2.circle(problem, (x,y), 5, (0,0,255), -1)
+        cv2.circle(problem, (x+w,y+h), 5, (0,0,255), -1)
+       
         return (x,y,w,h)
 
         
+    def drawSolvedNumbers(self,image,solvedNumbers):
+        (x,y,w,h) = points
+
     
+        
+
+        box_w = ( w ) // 9
+        box_h = ( h ) // 9
+
+      
+        for j in range(1,82):
+                if self.posArray[j-1]==1:
+
+                    cv2.putText(image, str(solvedNumbers[j-1]), (x+box_w//4,y+box_h//2), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0,0,255), 1, cv2.LINE_AA, False)
+                
+                x = x + box_w
+
+                if j%9==0:
+                    x = x - 9*box_w
+                    y = y + box_h
+        
+        return image
+
     def findNumberBoxes(self,image,points):
         (x,y,w,h) = points
 
-        """ 
-        x+=2
+        box_w = ( w ) // 9
+        box_h = ( h ) // 9
 
-        temp_x=x
-        temp_y=y
-        box_w = int((x+w+25)/10)
-        box_h = int((y+h-22)/10)
-        box_w = int((w-x)/8.2)
-        box_h = int((h-y)/7.5)  
-        
-        """
-
-        x += 2
-
-        temp_x = x
-        temp_y = y
-
-        box_w = int((x + w - 10) / 9)
-        box_h = int((y + h - 35) / 9)
-
-        for k in range(1,10):
-            for j in range(1,10):
-                cv2.rectangle(image, (x, y), (x + box_w, y + box_h), (np.random.randint(0,255), np.random.randint(0,255), np.random.randint(0,255)), 3)
+        for j in range(1,82):
+               
+                cv2.rectangle(image, (x, y), (x + box_w, y + box_h), (np.random.randint(0,255), np.random.randint(0,255), np.random.randint(0,255)), 2)
                 
                 self.findNumbers(image[y:y+box_h,x:x+box_w])
-
-
-
+                
                 x = x + box_w
 
-                if j%3==0:
-                    x = x - 3*box_w
+                if j%9==0:
+                    x = x - 9*box_w
                     y = y + box_h
             
-            y = temp_y
-            x = x + 3*box_w 
-
-            if k%3==0:
-                x = temp_x
-                
-                y = temp_y + 3*box_h
-                temp_y=y
-
+           
                 
             
                 
 
     def preprocessNumber(self,image):
         
-        
+
         image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
         
         image = cv2.resize(image,(32,32))
@@ -107,12 +102,43 @@ class SudokuClassifier:
 
         return image
 
+    def solve(self,):
+        numbers  = np.asarray(sc.numberList)
+        self.posArray = np.where(numbers > 0, 0, 1)
+
+        board = np.array_split(numbers,9)
+    
+        try:    
         
+            ss.solve(board)
+      
+        except Exception as e: 
+            print("Error solving problem: " + str(e))
+
+        return board
+    def drawNumbers(self,image):
+        flatList = []
+        for sublist in board:
+            for item in sublist:
+                flatList.append(item)
+ 
+        solvedNumbers =flatList*self.posArray
+
+        filled = sc.drawSolvedNumbers(image,solvedNumbers)
+        return filled
+       
     def findNumbers(self,image):
         
-        image = self.preprocessNumber(image[5:-5,5:-5])
         
+        cutX = 2*(image.shape[1]//10)
+        cutY = 2*(image.shape[0]//10)
 
+        image = self.preprocessNumber(image[cutY:-cutY,cutX:-cutX])
+        
+        #plt.imshow(image,cmap="gray")
+        #plt.show()
+        
+        
         if len(np.unique(image))==1:
             self.numberList.append(0)
         else:
@@ -121,7 +147,7 @@ class SudokuClassifier:
             classIndex = np.argmax(predictions,axis=1)
             probabilityValue = np.amax(predictions)
         
-            if probabilityValue > 0.78:
+            if probabilityValue > 0.50:
             
                 self.numberList.append(classIndex[0])
             else:
@@ -133,16 +159,24 @@ class SudokuClassifier:
 
 
 if __name__ == '__main__':
+    
     sc = SudokuClassifier()
      
     problem = cv2.imread('sudoku2.png')
-    
+  
     preprocessedImage = sc.preprocessSudoku(problem)
     points = sc.findBox(problem,preprocessedImage)
 
     sc.findNumberBoxes(problem,points)
-    print(np.reshape(sc.numberList,(9,3,3)))
+    
+
+    board = sc.solve()
+    
+    ss.print_board(board)
+    
+    filled = sc.drawNumbers(problem)
 
     cv2.imshow('preprocessedImage',preprocessedImage)
     cv2.imshow('problem',problem.copy())
+    cv2.imshow('filled',filled)
     cv2.waitKey(0)
